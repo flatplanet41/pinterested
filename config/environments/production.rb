e9c632dc71e9d4dd926979718ea0c31efa6c784f
John Elder <mail@flatplanetmedia.com>
2014-11-19 12:05:02 -0600

    added Devise and updated login logout pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #added as per devise installation instructions
+  config.action_mailer.default_url_options = { host: 'http://rails.elderacademy.com/' }
 end
